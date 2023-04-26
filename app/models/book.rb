b4cf1c26179823f8f7086577f212ef8ec069faaa
Elfin Git <musongoletracy@gmail.com>
2023-04-26 16:15:09 +0200

    edit book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :published, presence: true, numericality: { only_integer: true }
+    validates :genre, presence: true
+    validates :summary
+    has_one_attached :cover, dependent: :destroy
 end
